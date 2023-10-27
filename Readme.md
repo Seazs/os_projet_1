b852a173099ce33ad59393a801314826346d7f17
AlexBien <alexandre.bienfait@hotmail.com>
2023-10-27 17:31:13 +0200

    création du dépot et puload de tous les fichiers

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Readme
+
+## Description
+Projet 1 de Systeme d'exploitation
+
+## Installation
+pour pouvoir avoir les droits d'execution sur le script
+```shell
+chmod +x ./list-file.sh
+```
+
---
+++ list-file.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Vérifie si le nombre d'arguments est correct
+if [ "$#" -ne 1 ]; then
+    echo "Missing directory name." >&2
+    exit 1
+fi
+
+# Vérifie si le chemin spécifié est un dossier
+if [ ! -d "$1" ]; then
+    echo "The specified path is not a directory." >&2
+    exit 2
+fi
+
+# Liste les fichiers dans le dossier spécifié (ignorant les sous-dossiers)
+for file in $(ls -v "$1" 2>/dev/null); do
+    if [ -f "$file" ]; then
+        echo "$(basename "$file")"
+    fi
+done
+
+# Fin du script
