786e6c2cc345714b3dbfbccaa2831f71ce8d2859
AlexBien <alexandre.bienfait@hotmail.com>
2023-10-29 19:22:06 +0100

    sortie de l'image la plus proche,  mémoire partagé

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
+#define _GNU_SOURCE
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-#define IMG_DIST_PATH "./img-dist/img-dist"
+// Structure pour stocker les données partagées entre les processus
+struct shared_data {
+    int min_distance;
+    char min_distance_image_path[999];
+};
 
 // Fonction qui compare deux images et retourne la distance entre les deux
-int compare_image(char path_image1[999], char path_image2[999]);
+int compare_image(char path_image1[999], char path_image2[999], struct shared_data* data);
 
 int main(int argc, char* argv[]) {
     // Vérifier que le nombre d'arguments est correct
@@ -25,6 +32,17 @@ int main(int argc, char* argv[]) {
     // Variables pour stocker les chemins d'images
     char path_image1[999] = "";
     char path_image2[999] = "";
+
+    // Créer la région de mémoire partagée
+    struct shared_data* data = mmap(NULL, sizeof(struct shared_data), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON, -1, 0);
+    if (data == MAP_FAILED) {
+        perror("mmap()");
+        exit(1);
+    }
+
+    // Initialiser les données partagées
+    data->min_distance = 999999;
+    strcpy(data->min_distance_image_path, "");
 
     // Créer les deux processus fils
     pid_t pid1 = fork();
@@ -45,7 +63,7 @@ int main(int argc, char* argv[]) {
 
             // Comparer les images
             printf("pid1: %i\n", getpid());
-            int return_value = compare_image(image_to_compare_path, path_image1);
+            int return_value = compare_image(image_to_compare_path, path_image1, data);
             printf("La distance entre les deux images est de : %i\n", return_value);
         }
 
@@ -71,7 +89,7 @@ int main(int argc, char* argv[]) {
 
                 // Comparer les images
                 printf("pid2: %i\n", getpid());
-                int return_value = compare_image(image_to_compare_path, path_image2);
+                int return_value = compare_image(image_to_compare_path, path_image2, data);
                 printf("La distance entre les deux images est de : %i\n", return_value);
             }
 
@@ -82,13 +100,22 @@ int main(int argc, char* argv[]) {
             int status1, status2;
             waitpid(pid1, &status1, 0);
             waitpid(pid2, &status2, 0);
+
+            // Afficher le chemin de l'image avec la distance minimale
+            printf("Most similar image found: '%s' with a distance of %i.\n", data->min_distance_image_path, data->min_distance);
         }
+    }
+
+    // Détacher la région de mémoire partagée
+    if (munmap(data, sizeof(struct shared_data)) < 0) {
+        perror("munmap()");
+        exit(1);
     }
 
     return 0;
 }
 
-int compare_image(char path_image1[999], char path_image2[999]) {
+int compare_image(char path_image1[999], char path_image2[999], struct shared_data* data) {
     int return_value = -1;
     // Utilisation du programme img-dist qui prend en paramètre deux images et qui retourne la distance entre les deux
 
@@ -98,9 +125,12 @@ int compare_image(char path_image1[999], char path_image2[999]) {
         perror("fork()");
         return 1;
     } else if (pid == 0) {  // Processus fils
+        // Modifier le PATH pour inclure le répertoire de img-dist
+        setenv("PATH", "./img-dist", 1);
+
         // Exécution de la commande img-dist avec les deux chemins d'images en paramètres
-        char* argument_list[] = {IMG_DIST_PATH, path_image1, path_image2, NULL};
-        execvp(IMG_DIST_PATH, argument_list);
+        char* argument_list[] = {"img-dist", path_image1, path_image2, NULL};
+        execvp("img-dist", argument_list);
 
         // Afficher une erreur si l'exécution de la commande a échoué
         if (return_value < 0) {
@@ -116,6 +146,12 @@ int compare_image(char path_image1[999], char path_image2[999]) {
         } else {
             printf("Le fils s'est terminé de manière inattendue\n");
         }
+    }
+
+    // Mettre à jour la distance minimale et le chemin de l'image avec la distance minimale
+    if (return_value < data->min_distance) {
+        data->min_distance = return_value;
+        strcpy(data->min_distance_image_path, path_image2);
     }
 
     return return_value;
